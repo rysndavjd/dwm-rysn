fc717fe9dbb7134d1c6d94878af964e88a7c322f
rysndavjd <rysndavjd@protonmail.com>
2024-06-18 13:17:55 +0200

    fixed barsize

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const int user_bh            = 40;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 10;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "cantarell:size=40", "Symbols Nerd Font Mono:pixelsize=30" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
