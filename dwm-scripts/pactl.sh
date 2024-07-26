8bab17787bac815d909f728681abff955f409212
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2024-07-26 11:18:46 +0200

    added mute/unmute audio

--- dwm-scripts/pactl.sh
+++ dwm-scripts/pactl.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 endsh() {
-    if [[ "$1" == "notFound" ]]
-    then
-        exit 1
-    fi
+    exit 1
 }
 
 if ! command -v pactl &> /dev/null
 then
-    endsh "notFound"
+    endsh
 else
     if [[ "$1" == "inc" ]]
     then
@@ -19,6 +16,17 @@ else
     then
         pactl -- set-sink-volume 0 -10%
         exit 0
+    elif [[ "$1" == "mute" ]]
+    then
+        if [[ $(pactl get-sink-mute @DEFAULT_SINK@) == "Mute: no" ]]
+        then
+            pactl set-sink-mute @DEFAULT_SINK@ yes
+            exit 0
+        elif [[ $(pactl get-sink-mute @DEFAULT_SINK@) == "Mute: yes" ]]
+        then
+            pactl set-sink-mute @DEFAULT_SINK@ no
+            exit 0
+        fi
     fi
 fi
 
