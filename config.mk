654c16b1615f8805f69db3df4526c8b5652f0212
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2024-10-26 11:55:45 +0200

    dwm-6.5

--- config.mk
+++ config.mk
@@ -1,9 +1,11 @@
-VERSION = 1.3
+# dwm version
+VERSION = 6.5
+
+# Customize below to fit your system
 
 # paths
-PREFIX ?= /usr
+PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
-HELPERPREFIX ?= ${PREFIX}/share/dwm-rysn
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
@@ -15,16 +17,23 @@ XINERAMAFLAGS = -DXINERAMA
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = ${X11INC}/freetype2
+#MANPREFIX = ${PREFIX}/man
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
