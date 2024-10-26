1df21f78d6548ec1c3f5a110d7612255b337a495
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2024-10-26 13:20:11 +0200

    make

--- config.mk
+++ config.mk
@@ -1,11 +1,9 @@
-# dwm version
-VERSION = 6.5
-
-# Customize below to fit your system
+VERSION = 1.4
 
 # paths
-PREFIX = /usr/local
+PREFIX ?= /usr
 MANPREFIX = ${PREFIX}/share/man
+HELPERPREFIX ?= ${PREFIX}/share/dwm-rysn
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
