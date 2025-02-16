a3022706bd13c72984d53d98440b7ce47e342237
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2025-02-16 19:18:20 +0200

    copy startx

--- startdwm-rysn/startdwm-rysn.sh
+++ startdwm-rysn/startdwm-rysn.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/bin/sh
+
+unset SESSION_MANAGER
 
 d=0
 while true ; do
     [ -e "/tmp/.X$d-lock" -o -S "/tmp/.X11-unix/X$d" ] || break
     d=$(($d + 1))
 done
-display=":$d"
+defaultdisplay=":$d"
 unset d
+
+if [ "$display" = "" ]; then
+    display=$defaultdisplay
+fi
 
 xinit ~/startdwm-rysn -- :1
