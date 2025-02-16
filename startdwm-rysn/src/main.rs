6a0c12bfaa2a19a9ea46441037ed1e0c85f2300b
rysndavjd <rysndavjd@protonmail.com>
2025-02-16 17:24:53 +0200

    works

--- startdwm-rysn/src/main.rs
+++ startdwm-rysn/src/main.rs
@@ -1,10 +1,29 @@
-use std::{error::Error, thread, process::Command};
+use std::{error::Error, thread, process::Command, process::Stdio};
 use signal_hook::consts::signal::{SIGTERM, SIGINT, SIGQUIT};
 use signal_hook::iterator::Signals;
 use nix::sys::signal::{kill, Signal};
 use nix::unistd::Pid;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    /*
+    xrandr --output HDMI-0 --dpi 92 --rate 74.97 --primary --mode 1920x1080 --pos 0x0
+    wmname LG3D &
+    xset -dpms s off s noblank s 0 0 s noexpose &
+    xsettingsd &
+    xrdb -merge /home/rysndavjd/.Xresources &
+    gentoo-pipewire-launcher &
+    dbus-launch --sh-syntax --exit-with-session dwm
+     */
+    let exec: Vec<&str> = vec!["xrandr", "--output", "HDMI-0", "--dpi", "92", "--rate", "74.97", "--primary", "--mode", "1920x1080", "--pos", "0x0", "\0",
+                            "wmname", "LG3D", "\0", 
+                            "xset", "-dpms", "s", "off", "s", "noblank", "s", "0", "0", "s", "noexpose", "\0",
+                            "xsettingsd", "\0",
+                            "xrdb -merge /home/rysndavjd/.Xresources", "\0",
+                            "gentoo-pipewire-launcher", "\0",
+                            "dbus-launch", "--sh-syntax", "--exit-with-session", "dwm", "\0"];
+    #[cfg(debug_assertions)]
+    println!("{:?}", exec);
+
     let mut signals = Signals::new(&[
         SIGTERM,
         SIGINT,
@@ -18,9 +37,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let exec: Vec<&str> = vec!["sleep", "30", "\0", "sleep", "30", "\0", "sleep", "30", "\0"];
-    #[cfg(debug_assertions)]
-    println!("{:?}", exec);
     let mut program: Vec<&str> = Vec::new();
     let mut pids: Vec<u32> = Vec::new();
 
@@ -31,7 +47,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 for arg in program.iter().skip(1) {
                     command.arg(arg);
                 }
-                if let Ok(child) = command.spawn() {
+                if let Ok(child) = command.stdout(Stdio::piped()).stderr(Stdio::piped()).spawn() {
                     pids.push(child.id());
                 } else {
                     println!("{} command didn't start", program[0]);
