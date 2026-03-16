a320d52812a81d5abfebf865713116a1f1419bff
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2026-03-16 21:00:41 +0200

    patch dwm to be able to set cursor theme and default size via xcursor and added option to config.h and added dotfiles for all configurations of themes and startup

--- config-desktop.h
+++ config-desktop.h
@@ -1,24 +1,26 @@
 /* See LICENSE file for copyright and license details. */
-//Desktop
+// Desktop
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 16;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 1;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheel       = 0;
-static const int user_bh            = 5;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "cantarell:size=30", "Symbols Nerd Font Mono:pixelsize=35" };
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const unsigned int borderpx  		= 1;  /* border pixel of windows */
+static const unsigned int snap      		= 16; /* snap pixel */
+static const unsigned int systraypinning 	= 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft 	= 1;  /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing 	= 2;  /* systray spacing */
+static const int systraypinningfailfirst 	= 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        		= 1;  /* 0 means no systray */
+static const int showbar            		= 1;  /* 0 means no bar */
+static const int topbar             		= 1;  /* 0 means bottom bar */
+static const int focusonwheel       		= 0;
+static const int user_bh            		= 5;  /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const char *fonts[]          		= { "cantarell:size=30", "Symbols Nerd Font Mono:pixelsize=35" };
+static const char *cursortheme				= "Adwaita";
+static const unsigned int defaultcursorsize	= 40;
+static const char col_gray1[]       		= "#222222";
+static const char col_gray2[]       		= "#444444";
+static const char col_gray3[]       		= "#bbbbbb";
+static const char col_gray4[]       		= "#eeeeee";
+static const char col_cyan[]        		= "#005577";
+static const char *colors[][3]      		= {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
